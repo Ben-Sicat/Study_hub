8f47e86dfcff5bb31978c21bc5ab926478689612
ericramos-07 <ricjerome07@gmail.com>
2023-12-17 05:19:26 +0800

    Waitlist View (modal_wait)

--- frontend/app/components/modal_wait.tsx
+++ frontend/app/components/modal_wait.tsx
@@ -1,11 +1,17 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Butt from "./button";
-import DatePick from "./date_picker";
-import TimePick from "./time_picker";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import ModalExtend from "./modal_extend"; // Import your ModalExtend component
+import InfoTable from "./waitlist_table";
+
+type UserInfo = {
+  id: number;
+  name: string;
+  // Add more properties as needed
+};
 
 const style = {
   position: "absolute" as "absolute",
@@ -26,66 +32,30 @@ interface BasicModalProps {
 }
 
 function BasicModal({ isOpen, onClose }: BasicModalProps) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const router = useRouter();
+  const [showExtendModal, setShowExtendModal] = useState(false);
 
-  const [formData, setFormData] = useState<{
-    Date: string | any;
-    StartTime: string | any;
-    EndTime: string | any;
-  }>({
-    Date: "",
-    StartTime: "",
-    EndTime: "",
-  });
-
-  const handleInputChange = (field: string, value: string) => {
-    setFormData({
-      ...formData,
-      [field]: value,
-    });
+  const handleOpen = () => {
+    setShowExtendModal(true);
   };
-  const redirectUrl = "http://localhost:3000/qr_success_reservation"; //change this to a page after ng payment so magamit yung handleCreateAccount function. Dun pa dapat ma-ce-create yung reservation
-  const getName = "Gian"; //change get the name of user from session or local storage kung san man naka store
-  const tableFee = 140; //change den sa calculation
 
-  const handleCreateAccount = async () => {
-    try {
-      const apiData = {
-        chair_id: "", // Set a default value if not applicable
-        date: formData.Date,
-        starttime: formData.StartTime,
-        endtime: formData.EndTime,
-      };
-      router.push(
-        `https://payment-gateway-weld.vercel.app/gcash/login?amountDue=${tableFee}&merchant=Brew and Brains&redirectUrl=${redirectUrl}`
-      );
-
-      const response = await fetch(
-        "http://localhost:5000/api/create-reservation",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(apiData),
-        }
-      );
-
-      if (response.ok) {
-        // Successfully created account
-        console.log("Reserved successfully!");
-        // Optionally, you can redirect the user to a login page or another page
-      } else {
-        // Handle error cases
-        console.error("Error Reservation", await response.json());
-      }
-    } catch (error) {
-      console.error("Error Reservation", error);
-    }
+  const handleClose = () => {
+    setShowExtendModal(false);
+    onClose();
   };
+
+  const userData = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Smith" },
+    { id: 101, name: "Gian Limbaga" },
+    { id: 102, name: "The Fourth" },
+    { id: 103, name: "Melaissa Rioveros" },
+    { id: 104, name: "Chen Leonor" },
+    { id: 105, name: "Eric Ramos" },
+    // Add more user data as needed
+  ];
+
+  const [data, setData] = useState<UserInfo[]>(userData);
+  const [filteredData, setFilteredData] = useState<UserInfo[]>(data);
 
   return (
     <div>
@@ -97,41 +67,40 @@ function BasicModal({ isOpen, onClose }: BasicModalProps) {
       >
         <Box sx={style}>
           <div className="text-textcolor text-xl font-bold">
-            <h2>Ben Eric Trial</h2>
+            <h2>Waitlist View</h2>
           </div>
 
           <div className="container">
-            <div className="flex justify-center items-center mt-3">
-              <DatePick
-                text="Date:"
-                onInputChange={(value) => handleInputChange("date", value)}
-              ></DatePick>
+            <div className="container bg-gray-200 rounded-lg mt-7 mb-3 text-xs">
+              <InfoTable data={filteredData} />
             </div>
 
-            <div className="flex justify-center items-center mt-3">
-              <TimePick
-                text="Start Time:"
-                onInputChange={(value) => handleInputChange("starttime", value)}
-              ></TimePick>
-            </div>
-
-            <div className="flex justify-center items-center mt-3">
-              <TimePick
-                text="End Time:"
-                onInputChange={(value) => handleInputChange("endtime", value)}
-              ></TimePick>
+            <div className="flex justify-center space-x-5 text-xs">
+              <Butt
+                title="Exit"
+                Bgcolor="#EBE0D0"
+                width="160px"
+                height="30px"
+                borderRadius="10px"
+              />
+              <Butt
+                title="Enter"
+                Bgcolor="#EBE0D0"
+                width="160px"
+                height="30px"
+                borderRadius="10px"
+              />
             </div>
           </div>
-
-          <Butt
-            onClick={handleCreateAccount}
-            title="Reserve"
-            Bgcolor="#EBE0D0"
-            width="325px"
-            height="34px"
-          />
         </Box>
       </Modal>
+
+      {showExtendModal && (
+        <ModalExtend
+          isOpen={true /* or use a state variable for this */}
+          onClose={handleClose}
+        />
+      )}
     </div>
   );
 }
